7c2e5cf85173e68f07c0fea4878c13fccea1e1a6
Megan Smith <megans766@gmail.com>
2023-01-03 18:25:35 -0500

    set up data and view files

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ import express from "express"
 const app = express()
 
 //configure the app (app.set)
+app.set('view engine', 'ejs')
 
 //mount middleware (app.use)
 
 //mount routes
+app.get('/', function(req, res) {
+    res.render('home')
+})
 
 //tell the app to listen on port 3000
 app.listen(3000, function() {
